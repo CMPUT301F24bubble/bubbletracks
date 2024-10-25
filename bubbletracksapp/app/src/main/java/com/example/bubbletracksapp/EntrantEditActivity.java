b7b853f4f1751a0890184e3b9ff0dbca173e9bb4
Zoe Snyder <60548686+greenpicat@users.noreply.github.com>
2024-10-24 23:38:20 -0600

    Added entrant edit activity, where an entrant's profile can be edited

---
+++ bubbletracksapp/app/src/main/java/com/example/bubbletracksapp/EntrantEditActivity.java
@@ -0,0 +1,11 @@
+package com.example.bubbletracksapp;
+
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EntrantEditActivity extends AppCompatActivity {
+    TextView entrantNameInput = findViewById(R.id.entrantNameInput);
+    TextView entrantEmailInput = findViewById(R.id.entrantEmailInput);
+    TextView entrantPhoneInput = findViewById(R.id.entrantPhoneInput);
+}
