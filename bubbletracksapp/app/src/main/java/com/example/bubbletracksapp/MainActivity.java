710a6fa24c796363fde75bb595802f26f7a1879b
Zoe Snyder <60548686+greenpicat@users.noreply.github.com>
2024-10-29 17:05:38 -0600

    App is functional; opens up to load screen.

--- bubbletracksapp/app/src/main/java/com/example/bubbletracksapp/MainActivity.java
+++ bubbletracksapp/app/src/main/java/com/example/bubbletracksapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.bubbletracksapp;
 
 import android.os.Bundle;
 
+import com.example.bubbletracksapp.databinding.HomescreenBinding;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,7 +14,7 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-import com.example.bubbletracksapp.databinding.ActivityMainBinding;
+import com.example.bubbletracksapp.databinding.HomescreenBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
@@ -21,26 +22,15 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
+    private HomescreenBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        binding = HomescreenBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        setSupportActionBar(binding.toolbar);
-
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAnchorView(R.id.fab)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
