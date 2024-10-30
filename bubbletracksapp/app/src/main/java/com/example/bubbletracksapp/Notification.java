5aadc80c0c858551b556cd8eb7b169d93c8d9ebd
erza-t <erza@ualberta.ca>
2024-10-29 19:00:31 -0600

    Notification base code

--- bubbletracksapp/app/src/main/java/com/example/bubbletracksapp/Notification.java
+++ bubbletracksapp/app/src/main/java/com/example/bubbletracksapp/Notification.java
@@ -1,8 +1,108 @@
 package com.example.bubbletracksapp;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+
 /**
- * Send notifications to entrants
- * Organizers
+ * Organizer sends notifications to chosen entrants to sign up for events
+ *
  */
-public class Notification {
+public class Notification extends AppCompatActivity {
+    Button notif_button;
+    private static final String CHANNEL_ID = "my_channel";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        notif_button = (Button)findViewById(R.id.notify_button);
+        notif_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addNotification();
+            }
+        });
+    }
+    private void addNotification() {
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(this, CHANNEL_ID)
+
+                        .setSmallIcon(R.drawable.baseline_notifications_24)
+                        .setContentTitle("Notifications Example")
+                        .setContentText("This is a test notification");
+
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(contentIntent);
+
+        // Add as notification
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(0, builder.build());
+    }
 }
+    /*
+    private static final String CHANNEL_ID = "my_channel";
+    private static final int NOTIF_ID = 1;
+
+    private NotificationManager notificationManager ;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notify_attendees);
+
+        notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        createNotificationChannel();
+        Button notifyButton = findViewById(R.id.notify_button);
+        notifyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showNotification(view);
+            }
+        });
+    }
+
+
+    public void showNotification(View view) {
+        Intent intent = new Intent(this, Notification.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+        NotificationCompat.Builder builder = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                    .setSmallIcon(R.drawable.baseline_notifications_24)
+                    .setContentTitle("Notification")
+                    .setContentText("This is a notification")
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    .setContentIntent(pendingIntent)
+                    .setAutoCancel(true);
+
+        }
+
+        android.app.Notification notification;
+        notification = builder.build();
+        notificationManager.notify(NOTIF_ID, notification);
+    }
+
+    private void createNotificationChannel(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence channelName = "My Channel";
+            String channelDescription = "My Channel Description";
+
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, channelName, importance);
+            channel.setDescription(channelDescription);
+
+            notificationManager.createNotificationChannel(channel);
+        }
+    }*/
+
