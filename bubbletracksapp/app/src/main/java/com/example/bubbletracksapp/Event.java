4b52bae833808bae5361e20915755f5550071645
ChesterLuna <chester99124@gmail.com>
2024-11-01 01:46:45 -0600

    Event Time and Location getters

--- bubbletracksapp/app/src/main/java/com/example/bubbletracksapp/Event.java
+++ bubbletracksapp/app/src/main/java/com/example/bubbletracksapp/Event.java
@@ -3,9 +3,14 @@ package com.example.bubbletracksapp;
 import android.location.Location;
 import android.media.Image;
 
+import java.sql.Time;
+import java.util.Calendar;
+import java.util.Formatter;
+
 public class Event {
     private String name;
     private String description;
+    private Calendar date;
     //QRCode should probably be another type of field INCOMPLETE
     private String QRCode;
     private Location geolocation;
@@ -65,4 +70,38 @@ public class Event {
         return QRCode;
     }
 
+    public Calendar getDate() {
+        return date;
+    }
+
+    public void setDate(Calendar date) {
+        this.date = date;
+    }
+
+    public String getMonth() {
+        Formatter format = new Formatter();
+        format.format("%tb", date);
+        return format.toString();
+
+    }
+
+    public String getDay() {
+        Formatter format = new Formatter();
+        format.format("%tm", date);
+        return format.toString();
+
+    }
+
+    public String getTime() {
+        Formatter format = new Formatter();
+        format.format("%tl:%tM", date, date);
+        return format.toString();
+
+    }
+
+    //Should return a specific address
+    public String getLocation() {
+        return "Ualberta 10001";
+
+    }
 }
