be70c00969801e901cc17d58ce71877309597cda
ChesterLuna <chester99124@gmail.com>
2024-11-08 10:55:54 -0700

    Empty EntrantTest file

---
+++ bubbletracksapp/app/src/test/java/com/example/bubbletracksapp/EntrantTest.java
@@ -0,0 +1,224 @@
+package com.example.bubbletracksapp;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EntrantTest {
+
+    @Test
+    void toMapTest() {
+    }
+
+    /**
+     * Check if the Name of the mockEvent is the same as the Name we set.
+     */
+    @Test
+    void getNameTest() {
+    }
+
+    /**
+     * Check if the Name of the mockEvent is the same as the Name we set.
+     */
+    @Test
+    void getNameAsListTest() {
+    }
+
+    /**
+     * Check if the Name of the mockEvent is the same as the Name we set.
+     */
+    @Test
+    void getNameAsStringTest() {
+    }
+
+    /**
+     * Change the Name of the mockEvent and check if it changed.
+     */
+    @Test
+    void setNameTest() {
+    }
+
+    /**
+     * Check if the Email of the mockEvent is the same as the ID we set.
+     */
+    @Test
+    void getEmailTest() {
+    }
+
+    /**
+     * Change the Email of the mockEvent and check if it changed.
+     */
+    @Test
+    void setEmailTest() {
+    }
+
+    /**
+     * Check if the Email of the mockEvent is the same as the Email we set.
+     */
+    @Test
+    void getPhoneTest() {
+    }
+
+    /**
+     * Change the Phone of the mockEvent and check if it changed.
+     */
+    @Test
+    void setPhoneTest() {
+    }
+
+    /**
+     * Check if the Notification of the mockEvent is the same as the Notification we set.
+     */
+    @Test
+    void getNotificationTest() {
+    }
+
+    /**
+     * Change the Notification of the mockEvent and check if it changed.
+     */
+    @Test
+    void setNotificationTest() {
+    }
+
+    @Test
+    void testEqualsTest() {
+    }
+
+    @Test
+    void testHashCodeTest() {
+    }
+
+    /**
+     * Check if the ID of the mockEvent is the same as the ID we set.
+     */
+    @Test
+    void getIDTest() {
+    }
+
+    /**
+     * Change the ID of the mockEvent and check if it changed.
+     */
+    @Test
+    void setIDTest() {
+    }
+
+    /**
+     * Check if the EventsOrganized list of the mockEvent is the same as the EventsOrganized list we set.
+     */
+    @Test
+    void getEventsOrganizedTest() {
+    }
+
+    /**
+     * Change the EventsOrganized list of the mockEvent and check if it changed.
+     */
+    @Test
+    void setEventsOrganizedTest() {
+    }
+
+    /**
+     * Check if the EventsInvited list of the mockEvent is the same as the EventsInvited list we set.
+     */
+    @Test
+    void getEventsInvitedTest() {
+    }
+
+    /**
+     * Change the EventsInvited list of the mockEvent and check if it changed.
+     */
+    @Test
+    void setEventsInvitedTest() {
+    }
+
+    /**
+     * Check if the EventsEnrolled list of the mockEvent is the same as the EventsEnrolled list we set.
+     */
+    @Test
+    void getEventsEnrolledTest() {
+    }
+
+    /**
+     * Change the EventsEnrolled list of the mockEvent and check if it changed.
+     */
+    @Test
+    void setEventsEnrolledTest() {
+    }
+
+    /**
+     * Check if the EventsWaitlist list of the mockEvent is the same as the EventsWaitlist list we set.
+     */
+    @Test
+    void getEventsWaitlistTest() {
+    }
+
+    /**
+     * Change the EventsWaitlist list of the mockEvent and check if it changed.
+     */
+    @Test
+    void setEventsWaitlistTest() {
+    }
+
+    /**
+     * Add to List.
+     * Check the that the List contains the new ID
+     */
+    @Test
+    public void addToEventsOrganizedTest() {
+    }
+
+    /**
+     * Delete from list.
+     * Check the list does not contain the new ID
+     */
+    @Test
+    public void deleteFromEventsOrganizedTest() {
+    }
+
+    /**
+     * Add to List.
+     * Check the that the List contains the new ID
+     */
+    @Test
+    public void addToEventsInvitedTest() {
+    }
+
+    /**
+     * Delete from list.
+     * Check the list does not contain the new ID
+     */
+    @Test
+    public void deleteFromEventsInvitedTest() {
+    }
+
+    /**
+     * Add to List.
+     * Check the that the List contains the new ID
+     */
+    @Test
+    public void addToEventsEnrolledTest() {
+    }
+
+    /**
+     * Delete from list.
+     * Check the list does not contain the new ID
+     */
+    @Test
+    public void deleteFromEventsEnrolledTest() {
+    }
+
+    /**
+     * Add to List.
+     * Check the that the List contains the new ID
+     */
+    @Test
+    public void addToEventsWaitlistTest() {
+    }
+
+    /**
+     * Delete from list.
+     * Check the list does not contain the new ID
+     */
+    @Test
+    public void deleteFromEventsWaitlistTest() {
+    }
+}
