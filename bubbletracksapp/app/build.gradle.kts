4fdfd86c777a1068d16a7d461a09c1910448523f
smuir1 <semuir@ualberta.ca>
2024-11-08 10:28:31 -0700

    changed gradle for app (added an implementation that will not affect anybody else's work)

--- bubbletracksapp/app/build.gradle.kts
+++ bubbletracksapp/app/build.gradle.kts
@@ -53,4 +53,5 @@ dependencies {
     implementation(libs.picasso)
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.0.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.0.1")
+    implementation("com.github.rubensousa:gravitysnaphelper:2.2.2")
 }
