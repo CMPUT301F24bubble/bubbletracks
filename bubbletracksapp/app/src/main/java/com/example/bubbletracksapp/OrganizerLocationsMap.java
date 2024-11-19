120d5ea0cbad98aff3dfc7ff2acfeafd826a4ba6
ChesterLuna <chester99124@gmail.com>
2024-11-19 15:07:52 -0700

    Added javadocs to OrganizerLocationsMap

--- bubbletracksapp/app/src/main/java/com/example/bubbletracksapp/OrganizerLocationsMap.java
+++ bubbletracksapp/app/src/main/java/com/example/bubbletracksapp/OrganizerLocationsMap.java
@@ -20,6 +20,10 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
 
+/**
+ * Creates a map with the locations of the entrants of the given event.
+ * @author Chester
+ */
 public class OrganizerLocationsMap extends AppCompatActivity implements OnMapReadyCallback {
     private LocationsMapBinding binding;
     private Context context;
@@ -66,6 +70,12 @@ public class OrganizerLocationsMap extends AppCompatActivity implements OnMapRea
         });
     }
 
+    /**
+     * This function will run when the map is created and will populate it with
+     * markers that show the geolocations of the entrants that joined.
+     * @param googleMap The google map that will be populated with the Entrant's locations.
+     * @author Chester
+     */
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         entrantList.add(new Entrant());
@@ -82,6 +92,15 @@ public class OrganizerLocationsMap extends AppCompatActivity implements OnMapRea
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(centerPoint, 10));
     }
 
+    /**
+     * This method creates a marker in the given google map with the name of the
+     * given Entrant as it's title and the specific address of the Entrant
+     * as extra information.
+     *
+     * @param googleMap The Google Map where the marker will be created.
+     * @param entrant The Entrant from which the name and the address will be retrieved.
+     * @author Chester
+     */
     public void createMarker(GoogleMap googleMap, Entrant entrant) {
         LatLng markerPoint = entrant.getLocationPoint();
         String entrantName = entrant.getNameAsString();
