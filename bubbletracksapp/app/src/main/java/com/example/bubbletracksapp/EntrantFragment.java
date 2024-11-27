2baca29c86afd0965b93275ade35af5eb55ed7f5
Zoe Snyder <60548686+greenpicat@users.noreply.github.com>
2024-11-26 19:39:11 -0700

    Cleaned up default comments/parameters

--- bubbletracksapp/app/src/main/java/com/example/bubbletracksapp/EntrantFragment.java
+++ bubbletracksapp/app/src/main/java/com/example/bubbletracksapp/EntrantFragment.java
@@ -17,14 +17,7 @@ import android.widget.Button;
  */
 public class EntrantFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    //private static final String ARG_PARAM1 = "param1";
 
     public EntrantFragment() {
         // Required empty public constructor
@@ -34,16 +27,12 @@ public class EntrantFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment EntrantFragment.
      */
-    // TODO: Rename and change types and number of parameters
     public static EntrantFragment newInstance(String param1, String param2) {
         EntrantFragment fragment = new EntrantFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        //args.putString(ARG_PARAM1, param1);
         fragment.setArguments(args);
         return fragment;
     }
@@ -52,8 +41,7 @@ public class EntrantFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            //mParam1 = getArguments().getString(ARG_PARAM1);
         }
     }
 
