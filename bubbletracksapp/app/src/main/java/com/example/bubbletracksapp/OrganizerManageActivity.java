1601d1ef0d72922354202f89d272e82489938795
sgctcub <samyak1@ualberta.ca>
2024-11-28 23:53:16 -0700

    javadocs and comments for OrganizerManageActivity and Event

--- bubbletracksapp/app/src/main/java/com/example/bubbletracksapp/OrganizerManageActivity.java
+++ bubbletracksapp/app/src/main/java/com/example/bubbletracksapp/OrganizerManageActivity.java
@@ -10,31 +10,47 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+/**
+ * this class is an activity that allows organizers to manage their facility
+ * @author Samyak
+ * @version 1.0
+ */
 public class OrganizerManageActivity extends AppCompatActivity {
 
+    // declare the facility, id and location variables
     private Facility facility;
     private String id, location;
 
+    // declare the views necessary
     private ImageButton backButton;
     private TextView nameText, locationText;
     private Button createEventButton;
 
+    /**
+     * sets the layout, assigns all the views and sets up all the on click listeners
+     * @param savedInstanceState stores the state of the activity
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // set the layout
         setContentView(R.layout.facility_profile);
 
+        // assign the views
         backButton = findViewById(R.id.back_button);
         nameText = findViewById(R.id.facility_name_text);
         locationText = findViewById(R.id.facility_location_text);
         createEventButton = findViewById(R.id.create_event_button);
 
+        // get the id of facility
         Intent intent = getIntent();
         id = intent.getStringExtra("id");
 
+        // get the facility
         getFacility();
 
+        // handler to go back to home screen
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -43,9 +59,11 @@ public class OrganizerManageActivity extends AppCompatActivity {
             }
         });
 
+        // handler to create an event
         createEventButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // sends the location and id of the event
                 Intent createEventIntent = new Intent(OrganizerManageActivity.this, OrganizerActivity.class);
                 createEventIntent.putExtra("id", id);
                 createEventIntent.putExtra("location", location);
@@ -55,6 +73,9 @@ public class OrganizerManageActivity extends AppCompatActivity {
 
     }
 
+    /**
+     * gets the facility from the database
+     */
     protected void getFacility(){
         FacilityDB facilityDB = new FacilityDB();
         facilityDB.getFacility(id).thenAccept(curFacility -> {
@@ -71,6 +92,9 @@ public class OrganizerManageActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * sets the views given the facility
+     */
     protected void setViews(){
         nameText.setText(facility.getName());
         locationText.setText(facility.getLocation());
